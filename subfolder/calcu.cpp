b1c6ecccd2cb44f7f592863a8d7c073d2af19085
AkshatMishra01 <mishraakshat924@gmail.com>
2020-02-25 16:26:48 +0530

    commitedd

---
+++ subfolder/calcu.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class parent
+{
+  public:
+	  int 
+		  id_sap;
+	  sap()
+	  {
+	   cout<<"welcome to the id function";
+
+	  }
+};
+int mian()
+{
+parent o1;
+o1.sap();
+return 0;
+}
